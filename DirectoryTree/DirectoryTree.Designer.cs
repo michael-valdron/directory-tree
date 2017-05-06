97875efe380a393296f7d5886126bb3016586146
Michael Valdron <michael.valdron@gmail.com>
2017-05-05 23:20:57 -0400

    Initial commit.

---
+++ DirectoryTree/DirectoryTree.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace DirectoryTree
+{
+    partial class DirectoryTree
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tvDirView = new System.Windows.Forms.TreeView();
+            this.imglstDirIco = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // tvDirView
+            // 
+            this.tvDirView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tvDirView.Location = new System.Drawing.Point(0, 0);
+            this.tvDirView.Name = "tvDirView";
+            this.tvDirView.Size = new System.Drawing.Size(354, 481);
+            this.tvDirView.TabIndex = 2;
+            // 
+            // imglstDirIco
+            // 
+            this.imglstDirIco.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imglstDirIco.ImageSize = new System.Drawing.Size(16, 16);
+            this.imglstDirIco.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // DirectoryTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tvDirView);
+            this.Name = "DirectoryTree";
+            this.Size = new System.Drawing.Size(354, 481);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView tvDirView;
+        private System.Windows.Forms.ImageList imglstDirIco;
+    }
+}
---
+++ DirectoryTree/DirectoryTree.cs
@@ -0,0 +1,117 @@
+﻿/* DirectoryTree
+ * =============
+ * 
+ * Author: Michael Valdron
+ * Build: 1.0
+ * Licence: MIT 
+ * Copyright (c) 2017 Michael Valdron
+ */
+
+using System.Windows.Forms;
+using System.IO;
+
+namespace DirectoryTree
+{
+    public partial class DirectoryTree : UserControl
+    {
+        private TreeNode currNode;
+
+        public DirectoryTree()
+        {
+            InitializeComponent();
+        }
+
+        public TreeView Tree
+        {
+            get
+            {
+                return tvDirView;
+            }
+        }
+
+        public TreeNode SelectedNode
+        {
+            get
+            {
+                return currNode;
+            }
+            set
+            {
+                currNode = value;
+            }
+        }
+
+        public TreeNode buildDir(DirectoryInfo dirInfo)
+        {
+            TreeNode topNode;
+
+            topNode = buildDirParents(dirInfo, tvDirView.Nodes);
+
+            buildSubDirs(dirInfo, topNode.Nodes);
+
+            return topNode;
+        }
+
+        private TreeNode buildDirParents(DirectoryInfo dirInfo, TreeNodeCollection tree)
+        {
+            TreeNode selNode;
+
+            if (dirInfo.FullName == dirInfo.Root.FullName)
+            {
+                return tree.Add(dirInfo.Name);
+            }
+            else
+            {
+                selNode = buildDirParents(dirInfo.Parent, tree);
+                return selNode.Nodes.Add(dirInfo.Name);
+            }
+        }
+
+        private void buildSubDirs(DirectoryInfo dirInfo, TreeNodeCollection tree)
+        {
+            foreach (DirectoryInfo subDir in dirInfo.GetDirectories())
+            {
+                tree.Add(subDir.Name);
+            }
+        }
+
+        public DirectoryInfo findParentNode(DirectoryInfo currDir, TreeNode tarNode)
+        {
+            if (currDir.Name.Equals(tarNode.Text))
+            {
+                return currDir;
+            }
+            else if (currDir.Parent == null)
+            {
+                return null;
+            }
+            else
+            {
+                return findParentNode(currDir.Parent, tarNode);
+            }
+        }
+
+        public DirectoryInfo findChildNode(DirectoryInfo currDir, TreeNode tarNode)
+        {
+            DirectoryInfo result = null;
+
+            foreach (DirectoryInfo subDir in currDir.GetDirectories())
+            {
+                if (subDir.Name.Equals(tarNode.Text))
+                {
+                    return subDir;
+                }
+            }
+
+            return result;
+        }
+
+        public void removeNodes()
+        {
+            foreach (TreeNode aNode in tvDirView.Nodes)
+            {
+                aNode.Remove();
+            }
+        }
+    }
+}
