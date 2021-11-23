95464d7715664978db1cec83ef86c0f22fedc1e4
Michael Valdron <michael.valdron@gmail.com>
2021-11-23 03:03:25 -0500

    DirectoryUtils added.

---
+++ DirectoryTree/DirectoryUtils.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace DirectoryTree
+{
+    public sealed class DirectoryUtils
+    {
+        public static string TrimPathSep(string path)
+        {
+            var sep = Path.DirectorySeparatorChar;
+            var rootIdent = (sep == '\\') ? ":" : "";
+            
+            if (Path.IsPathRooted(path))
+                return Regex.Replace(path, $"{rootIdent}\\{sep}", rootIdent);
+            else
+                return path;
+        }
+    }
+}
